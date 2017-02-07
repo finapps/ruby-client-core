bd69512779a9ed6e51c1b19063fbf3b1e5bcd5c0
Erich Quintero <erich@financialapps.com>
2017-02-07 16:49:47 -0500

    rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 if ENV['CODECLIMATE_REPO_TOKEN']
   # require 'codeclimate-test-reporter'
   # CodeClimate::TestReporter.start
-  require "simplecov"
+  require 'simplecov'
   SimpleCov.start
 end
 
