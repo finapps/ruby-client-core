d6ac4002fdd1aeee745e5d2dac053b80cc898084
Erich Quintero <erich@financialapps.com>
2017-02-07 16:48:33 -0500

    gem upgrades

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
+  # require 'codeclimate-test-reporter'
+  # CodeClimate::TestReporter.start
+  require "simplecov"
+  SimpleCov.start
 end
 
 require 'bundler/setup'
