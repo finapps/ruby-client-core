969080c967536d607b730b804139a66c9166625d
Erich Quintero <qbantek@gmail.com>
2017-10-17 17:27:18 -0400

    test resources

--- spec/rest/resources_spec.rb
+++ spec/rest/resources_spec.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
+require 'spec_helpers/client'
+
 RSpec.describe FinAppsCore::REST::Resources do
+  include SpecHelpers::Client
+  subject { FinAppsCore::REST::Resources.new client }
+
   describe '#new' do
+    context 'for a valid client param' do
+      it { expect { subject }.not_to raise_error }
+    end
+
+    context 'when missing client param' do
+      subject { FinAppsCore::REST::Resources.new nil }
+      it { expect { subject }.to raise_error(FinAppsCore::MissingArgumentsError) }
+    end
   end
+
   describe '#list' do
   end
   describe '#create' do
