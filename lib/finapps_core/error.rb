94569777036ef1b50781980f93b3cd5a7db75592
Anthony Mahadeo <anthony@financialapps.com>
2019-05-16 10:13:15 -0400

    Create UserLockoutError

--- lib/finapps_core/error.rb
+++ lib/finapps_core/error.rb
@@ -23,6 +23,9 @@ module FinAppsCore # :nodoc:
   # Raised whenever the connection fails.
   class ConnectionFailedError < Error; end
 
+  # Raised whenever the user is locked out from sign in.
+  class UserLockoutError < Error; end
+
   %i[InvalidArgumentsError MissingArgumentsError ApiSessionTimeoutError
      UnsupportedHttpMethodError ConnectionFailedError].each do |const|
     Error.const_set(const, FinAppsCore.const_get(const))
