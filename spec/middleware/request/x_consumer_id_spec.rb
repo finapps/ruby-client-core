1b1f3e4498cd37735ecd9ba2dc9c3c9ac1872294
Sarah Keller <sarah@financialapps.com>
2020-02-11 10:34:49 -0500

    Add consumer id spec

---
+++ spec/middleware/request/x_consumer_id_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+RSpec.describe FinAppsCore::Middleware::XConsumerId do
+  let(:key) { FinAppsCore::Middleware::XConsumerId::KEY }
+  let(:id) { 'valid_consumer_id' }
+  let(:fake_app) { proc {|env| env } }
+  let(:env) { { request_headers: {} } }
+
+  describe '#call' do
+    subject { FinAppsCore::Middleware::XConsumerId.new(fake_app, id) }
+
+    it('generates an X-Consumer-ID header') do
+      expect(subject.call(env)[:request_headers][key]).to eq(id)
+    end
+  end
+end
