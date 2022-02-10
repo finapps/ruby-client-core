f4a849769c83465c753b81b8301f5f81f75c332d
Erich N Quintero <qbantek@gmail.com>
2022-02-10 15:05:54 -0500

    Log JSON response not ruby hash (#73)
    * Log json response not converted hash
    
    * Disable Gemspec/RequireMFA cop
    
    * Run and fix latest rubocop warnings
    
    * Test only on current ruby

--- lib/finapps_core/rest/connection.rb
+++ lib/finapps_core/rest/connection.rb
@@ -19,13 +19,13 @@ module FinAppsCore
       end
 
       def init_connection_response(conn, logger)
+        conn.response :json,
+                      content_type: /\bjson$/,
+                      parser_options: {symbolize_names: true}
         conn.response :logger, logger, bodies: true,
                                        headers: true,
                                        formatter: FinAppsCore::Logging::ContenTypeFormatter,
                                        log_level: :debug
-        conn.response :json,
-                      content_type: /\bjson$/,
-                      parser_options: {symbolize_names: true}
       end
 
       def init_connection_request(conn, config)
--- spec/support/fake_api.rb
+++ spec/support/fake_api.rb
@@ -34,7 +34,7 @@ class FakeApi < Sinatra::Base
   def json_response(response_code, file_name)
     content_type :json
     status response_code
-    File.open("#{File.dirname(__FILE__)}/fixtures/#{file_name}", 'rb').read
+    File.binread("#{File.dirname(__FILE__)}/fixtures/#{file_name}")
   end
 
   def version; end
